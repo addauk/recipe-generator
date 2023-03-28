587843ff3a266cd0e98c6c0696a975a6927327da
Shamima14 <sbegum91@gmail.com>
2023-03-28 16:49:54 +0100

    added allRecipes, recipe and updated code in Ingredients.js

---
+++ frontend/src/components/allRecipes/AllRecipes.js
@@ -0,0 +1,21 @@
+import Recipe from "../recipe/Recipe";
+import React from "react";
+
+const AllRecipes = (props) => {
+  return (
+    <div>
+      {props.recipes.map((recipe, index) => (
+        <Recipe
+          key={index}
+          src={recipe.image}
+          food={recipe.title}
+          cookingTime={recipe.cookingTime}
+          rating={recipe.rating}
+          image={"2"}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default AllRecipes;
--- frontend/src/components/ingredient/Ingredient.js
+++ frontend/src/components/ingredient/Ingredient.js
@@ -1,14 +1,21 @@
 import React, { useState, useEffect } from "react";
+import Recipe from "../recipe/Recipe";
+import AllRecipes from "../allRecipes/AllRecipes";
 
 const Ingredient = ({navigate}) => {
   const ingredientList = ["Chicken", "Turkey", "Bacon", "Pepper", "Potato", "Milk", "Cheese", "Beef",  "Pork",  "Fish",  "Shrimp",  "Rice",  "Pasta", "Carrots", "Mushroom", "Onion"];
   const recipeList = [{
-    title: "Chicken Casserole",
+    image: "https://img.sndimg.com/food/image/upload/w_555,h_416,c_fit,fl_progressive,q_95/v1/img/recipes/39/picM9Mhnw.jpg",
+    cookingTime: "60",
+    rating: "3",
     ingredients: ["Chicken", "Carrots", "Mushroom", "Flour", "Garlic", "Celery", "Onion"],
     instructions: "Heat a knob of butter and ½ tbsp rapeseed or olive oil in a large frying pan."
   },
   {
+    image: "https://img.sndimg.com/food/image/upload/w_555,h_416,c_fit,fl_progressive,q_95/v1/img/recipes/47/picfnmxck.jpg", 
     title: "Beef lasagne",
+    cookingTime: "90",
+    rating: "4",
     ingredients: ["Beef", "Onion", "Tomato", "Celery", "Stock", "Pasta", "Milk", "Cheese"],
     instructions: "To make the Bolognese, heat the oil in a large heavy based saucepan over medium heat."
   }]
@@ -48,10 +55,10 @@ const handleCheck = (event) => {
 
  let isChecked = (item) =>
     checked.includes(item) ? "checked-item" : "not-checked-item";
-  
+
 const handleSubmit = (event) => {
   event.preventDefault();
-  if (checked.length === 0) { 
+  if (checked.length === 0) {
     throw ("No Items Checked");
   }else {
     const matchedRecipes = findRecipe();
@@ -65,7 +72,7 @@ return (
   <div className="ingredient-header"></div>
   <div className="list-container">
     <h2>Ingredients</h2>
-    
+
       {ingredientList.map((item, index) => (
          <div key={index}>
          <input value={item} type="checkbox" onChange={handleCheck} />
@@ -79,13 +86,7 @@ return (
             {matchedRecipes.length > 0 && (
           <div className="matched-recipes">
             <h2>Matched Recipes</h2>
-              {matchedRecipes.map((recipe, index) => (
-                <div key={index}>
-                  <h3>{recipe.title}</h3>
-                <ul>{recipe.ingredients.join(", ")}</ul>
-                <p>{recipe.instructions}</p>
-                </div>
-              ))}
+            <AllRecipes recipes={matchedRecipes} />
           </div>
         )}
   </div>
@@ -93,3 +94,11 @@ return (
 )};
 
 export default Ingredient;
+
+// {matchedRecipes.map((recipe, index) => (
+//   <div key={index}>
+//     <h3>{recipe.title}</h3>
+//   <ul>{recipe.ingredients.join(", ")}</ul>
+//   <p>{recipe.instructions}</p>
+//   </div>
+// })
