44f98f9b088242e0bb8f404f79665a8e794f68c0
Max Allan-Smith <Maxibon_1195@hotmail.com>
2023-04-03 12:41:50 +0100

    Sorry

--- frontend/src/components/app/App.js
+++ frontend/src/components/app/App.js
@@ -8,12 +8,12 @@ import UserPage from "../profile/UserPage";
 import RecipeDetail from "../recipeDetail/RecipeDetail";
 
 function App() {
-  const [currentUser, setCurrentUser] = useState(null); 
+  const [currentUser, setCurrentUser] = useState(null);
 
   return (
     <Routes>
       <Route path="/" element={<Ingredient navigate={useNavigate()} />} />
-           <Route path="/login" element={<LoginForm navigate={useNavigate()} setCurrentUser={setCurrentUser} />} />
+      <Route path="/login" element={<LoginForm navigate={useNavigate()} setCurrentUser={setCurrentUser} />} />
       <Route path="/signup" element={<SignUpForm navigate={useNavigate()} setCurrentUser={setCurrentUser} />} />
       <Route
         path="/users/:id"
