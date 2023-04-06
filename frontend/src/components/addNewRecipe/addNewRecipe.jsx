0c1f3eddade95866b6cd9c913d3599713bb8fe07
Max Allan-Smith <Maxibon_1195@hotmail.com>
2023-04-06 01:43:58 +0100

    Finished the design of page and its functional

--- frontend/src/components/addNewRecipe/addNewRecipe.jsx
+++ frontend/src/components/addNewRecipe/addNewRecipe.jsx
@@ -1,8 +1,6 @@
 import { Component } from "react";
 import '../addNewRecipe/addNewRecipe.css'
 import axios from "axios";
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
 
 class AddNewRecipe extends Component {
   constructor(props) {
@@ -30,9 +28,13 @@ class AddNewRecipe extends Component {
     TotalTimeHours: '',
     TotalTime: '',
     RecipeCategory: 'Breakfast',
-    RecipeServings: '',
+    RecipeServings: '1',
     ImageLinks: [],
-    showModal: false,
+   }
+
+   routeChange = () => {
+    const path = `/`; 
+    this.navigate(path);
    }
 
    handleCookTime = () => {
@@ -95,15 +97,14 @@ class AddNewRecipe extends Component {
    }
 
    handleAddInstruction = () => {
-    const newInstruction = this.state.instructionInputValue.trim();
-    if (newInstruction !== "" && !this.state.Instructions.includes(newInstruction)) {
-      const newInstruction = "";
+    const newInstructionValue = this.state.instructionInputValue.trim();
+    if (newInstructionValue !== "" && !this.state.Instructions.includes(newInstructionValue)) {
       this.setState(prevState => ({
-        Instructions: [...prevState.Instructions, newInstruction],
+        Instructions: [...prevState.Instructions, newInstructionValue],
         instructionInputValue: ""
       }));
     }
-   }
+  }
 
    handleRemoveInstruction = (index) => {
     const newInstruction = [...this.state.Instructions];
@@ -119,17 +120,6 @@ class AddNewRecipe extends Component {
 
   async handleSubmit(event) {
     event.preventDefault();
-
-    // const userName = JSON.parse(window.localStorage.getItem('userData')).userName;
-    // if (userName !== "" && !this.state.AuthorName.includes(userName)) {
-    //   const userName = "";
-    //   this.setState(prevState => ({
-    //     AuthorName: [...prevState.AuthorName, userName]
-    //   }));
-    // }
-    // console.log(`Username: ${userName}`);
-    // this.setState({AuthorName: `${userName}`});
-    // console.log(this.state);
 
     await axios.post("/recipe/new", this.state)
       .then(response => {
@@ -164,13 +154,21 @@ class AddNewRecipe extends Component {
           <label htmlFor="recipe-name">Recipe Name:</label><br/>
           <input required type="text" id="recipe-name" name="recipe-name" value={this.state.Name} onChange={(event) => this.setState({Name: event.target.value})}/><br/>
 
+          <br />
+          <hr />
+          <br />
+
           <label htmlFor="recipe-description">Recipe Description:</label><br/>
           <textarea required id="recipe-description" name="recipe-description" value={this.state.Description} onChange={(event) => this.setState({Description: event.target.value})}></textarea><br/>
+
+          <br />
+          <hr />
+          <br />
           
           {/* Add Ingredients */}
-          <label className="ingredientsLabel" htmlFor="ingredients">Ingredients:</label><br/>
           {this.state.Ingredients.map((ingredient, index) => (
                 <div className="addedIngedientContainer" key={index}>
+                  <br />
                   <div className="ingredientsAdded">
                     <li className="ingredientName">{ingredient}</li>
                   </div>
@@ -195,59 +193,105 @@ class AddNewRecipe extends Component {
           <input className="addIngredientTextbox" type="text" id="ingredients" name="ingredients" value={this.state.ingredientInputValue} onChange={(event) => this.setState({ingredientInputValue: event.target.value})} placeholder="Add Ingredient Here"/><br/>
           <button className="addIngredientButton" onClick={this.handleAddIngredient}>Add Ingredient</button>
 
-
-          {/* Add Instructions */}
-          <label className="instructionLabel" htmlFor="instructions">Instructions:</label><br/>
-          {this.state.Instructions.map((instruction, index) => (
-            <div className="addedInstructionsContainer" key={index}>
-              <div className="instrcutionsAdded">
-                <li className="instructionName">{instruction}</li>
-              </div>
-                <button className="removeInstructionButton" onClick={() => this.handleRemoveInstruction(index)}>Remove Instruction</button>
-              </div>
-          ))}
-
-          <input className="addInstructionsTextbox" type="text" id="instructions" name="instructions" value={this.state.instructionInputValue} onChange={(event) => this.setState({instructionInputValue: event.target.value})} placeholder="Add Instructions Here"/><br/>
-          <button className="addInstructionsButton" onClick={this.handleAddInstruction}>Add Instruction</button>
-          <br></br>
+          <br />
           <hr />
           <br />
+
+
+          {/* Add Instructions */}
+          {this.state.Instructions.map((instruction, index) => {
+            return (
+              <div className="add-instruction-item" key={index}>
+                <li>
+                  <span className="instruction-item">{instruction}</span>
+                </li>
+              </div>
+            );
+          })}
+          <div className="add-instructions-container">
+            <input className="addInstructionsTextbox" type="text" id="instructions" name="instructions" value={this.state.instructionInputValue} onChange={(event) => this.setState({instructionInputValue: event.target.value})} placeholder="Add Instructions Here"/>
+            <br/>
+            <br />
+            <button className="addInstructionsButton" onClick={this.handleAddInstruction}>Add Instruction</button>
+          </div>
+
+
+          <br />
+          <hr />
+          <br />
+
           {/* Cooking time container */}
           <div className="cooking-time-container">
             <label htmlFor="cooking-time">Cooking Time:</label><br/>
-            <input required type="text" id="cooking-time-hours" name="cooking-time-hours" value={this.state.CookTimeHours} onChange={(event) => this.setState({CookTimeHours: event.target.value})} placeholder="Cook Time Minutes"/><br/>
-            <input required type="text" id="cooking-time-minutes" name="cooking-time-minutes" value={this.state.CookTimeMinutes} onChange={(event) => this.setState({CookTimeMinutes: event.target.value})} placeholder="Cook Time Hours"/><br/>
+            <input required type="text" id="cooking-time-hours" name="cooking-time-hours" value={this.state.CookTimeHours} onChange={(event) => this.setState({CookTimeHours: event.target.value})} placeholder="Cook Time Hours"/><br/>
+            <input required type="text" id="cooking-time-minutes" name="cooking-time-minutes" value={this.state.CookTimeMinutes} onChange={(event) => this.setState({CookTimeMinutes: event.target.value})} placeholder="Cook Time Minutes"/><br/>
           </div>
-            <br></br>
+
+          <br />
+          <br />
+
           {/* Preparation time container */}
           <div className="preparation-time-container">
             <label htmlFor="preparation-time">Preparation Time:</label><br/>
-            <input required type="text" id="preparation-time-hours" name="preparation-time-hours" value={this.state.PrepTimeHours} onChange={(event) => this.setState({PrepTimeHours: event.target.value})} placeholder="Prep Time Minutes"/><br/>
-            <input required type="text" id="preparation-time-minutes" name="preparation-time-minutes" value={this.state.PrepTimeMinutes} onChange={(event) => this.setState({PrepTimeMinutes: event.target.value})} placeholder="Prep Time Hours"/><br/>
+            <input required type="text" id="preparation-time-hours" name="preparation-time-hours" value={this.state.PrepTimeHours} onChange={(event) => this.setState({PrepTimeHours: event.target.value})} placeholder="Prep Time Hours"/><br/>
+            <input required type="text" id="preparation-time-minutes" name="preparation-time-minutes" value={this.state.PrepTimeMinutes} onChange={(event) => this.setState({PrepTimeMinutes: event.target.value})} placeholder="Prep Time Minutes"/><br/>
           </div>
-          <br></br>
+
+          <br />
+
           {/* Total time container */}
           <div className="calculate-totalTime-container">
             <button className="calculate-totalTime-button" onClick={this.handleTotalTime}>Calculate Total Time</button>
           </div>
+
           <br />
           <hr />
-          <br></br>
-          <label htmlFor="recipe-category">Recipe Category:</label><br/>
-          <select required id="recipe-category" name="recipe-category" value={this.state.RecipeCategory} onChange={(event) => this.setState({RecipeCategory: event.target.value})}>
-            <option value="breakfast">Breakfast</option>
-            <option value="lunch">Lunch</option>
-            <option value="dinner">Dinner</option>
-            <option value="dessert">Dessert</option>
-          </select><br/>
+          <br />
 
-          <label htmlFor="recipe-servings">Recipe Servings:</label><br/>
-          <input type="text" id="recipe-servings" name="recipe-servings" value={this.state.RecipeServings} onChange={(event) => this.setState({RecipeServings: event.target.value})}/><br/>
+          <div className="recipe-category-container">
+            <label htmlFor="recipe-category">Recipe Category:</label>
+            <br/>
+            <br />
+            <select required id="recipe-category" name="recipe-category" value={this.state.RecipeCategory} onChange={(event) => this.setState({RecipeCategory: event.target.value})}>
+              <option value="breakfast">Breakfast</option>
+              <option value="lunch">Lunch</option>
+              <option value="dinner">Dinner</option>
+              <option value="dessert">Dessert</option>
+            </select><br/>
+          </div>
+
+          <br />
+          <hr />
+          <br />
+
+          <div className="recipe-servings-container">
+            <label htmlFor="recipe-servings">Recipe Servings:</label>
+            <br/>
+            <br />
+            <select required className="recipe-servings" id="recipe-servings" name="recipe-servings" value={this.state.RecipeServings} onChange={(event) => this.setState({RecipeServings: event.target.value})}>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select><br/>
+          </div>
+
+          <br />
+          <hr />
+          <br />
 
           <label htmlFor="image-upload">Image Upload:</label><br/>
           <input type="file" id="image-upload" name="image-upload" multiple value={this.state.ImageLinks} onChange={(event) => this.setState({ImageLinks: event.target.value})}/><br/>
 
-          <button type="button" value="Submit" onClick={this.handleSubmit}>Submit Recipe</button>
+          <div className="submit-button-container">
+            <button type="button" value="Submit" onClick={this.handleSubmit} href="/">Submit Recipe</button>
+          </div>
         </form>
       </div>
     );
